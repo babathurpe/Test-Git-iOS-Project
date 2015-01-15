a9c0445f269fab96738924c60322327827fc37d2
Babatope <babathurpe@yahoo.com>
2015-01-15 12:12:49 -0500

    Added TestFile.h and TestFile.m

---
+++ GitTest/TestFile.h
@@ -0,0 +1,13 @@
+//
+//  TestFile.h
+//  GitTest
+//
+//  Created by Babathurpe on 2015-01-15.
+//  Copyright (c) 2015 Babathurpe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestFile : NSObject
+
+@end
---
+++ GitTest/TestFile.m
@@ -0,0 +1,13 @@
+//
+//  TestFile.m
+//  GitTest
+//
+//  Created by Babathurpe on 2015-01-15.
+//  Copyright (c) 2015 Babathurpe. All rights reserved.
+//
+
+#import "TestFile.h"
+
+@implementation TestFile
+
+@end
